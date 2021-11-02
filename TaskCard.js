90bd3b38610fc6eb9c1461599aa85250fe957fab
Seth Bone <seth5124@gmail.com>
2021-11-02 00:06:55 -0400

    Added edit element function

--- TaskCard.js
+++ TaskCard.js
@@ -1,19 +1,29 @@
 export function TaskCard(task) {
-  let taskDueDateFormatted = `${task.dueDate.getMonth()}/${task.dueDate.getDate() + 1}`;
+  let taskDueDateFormatted = `${task.dueDate.getMonth()}/${
+    task.dueDate.getDate() + 1
+  }`;
 
   //Creates task card
   let taskCard = document.createElement("div");
 
   //h1 tag for task title and due date
-  let taskHeader = document.createElement('div');
+  let taskHeader = document.createElement("div");
   let taskTitle = document.createElement("h1");
-  taskTitle.classList.add('taskHeaderElement');
+  taskTitle.classList.add("taskHeaderElement");
+  taskTitle.addEventListener('dblclick',()=>{
+    editElement(taskTitle);
+  })
   taskTitle.innerHTML = `${task.name}`;
-  let taskDueDate = document.createElement('h1');
-  taskDueDate.classList.add('taskHeaderElement');
+  let taskDueDate = document.createElement("h1");
+  taskDueDate.classList.add("taskHeaderElement");
   taskDueDate.innerHTML = `${taskDueDateFormatted}`;
   taskHeader.appendChild(taskTitle);
-  taskHeader.append(Object.assign(document.createElement('h1'),{innerHTML: ' - ', className: 'taskHeaderElement'}));
+  taskHeader.append(
+    Object.assign(document.createElement("h1"), {
+      innerHTML: " - ",
+      className: "taskHeaderElement",
+    })
+  );
   taskHeader.appendChild(taskDueDate);
   taskCard.appendChild(taskHeader);
 
@@ -29,21 +39,21 @@ export function TaskCard(task) {
   let notesHeader = document.createElement("h4");
   notesHeader.innerHTML = "Notes";
 
-  let notesEditButton = document.createElement("img");
-  notesEditButton.src = "./assets/pencil.svg";
-  notesEditButton.classList.add("pencilIcon");
-  notesEditButton.addEventListener("click", (e) => {
+  let newNoteButton = document.createElement("img");
+  newNoteButton.src = "./assets/pencil.svg";
+  newNoteButton.classList.add("pencilIcon");
+  newNoteButton.addEventListener("click", (e) => {
     let newNoteBox = document.createElement("input");
     let taskNoteList = document.getElementById("taskNoteList");
     taskNoteList.insertBefore(newNoteBox, taskNoteList.firstChild);
     newNoteBox.addEventListener("keyup", (event) => {
       if (event.key === "Enter") {
         task.addNote(newNoteBox.value);
-        taskNoteList.replaceWith(loadNotes(task));
+        document.getElementById("taskNoteList").replaceWith(loadNotes(task));
       }
     });
   });
-  notesHeader.appendChild(notesEditButton);
+  notesHeader.appendChild(newNoteButton);
 
 
   notesListContainer.appendChild(notesHeader);
@@ -75,23 +85,44 @@ function loadNotes(task) {
     let taskNote = document.createElement("li");
     taskNote.classList.add("taskNote");
     taskNote.innerHTML = notes[note];
+    taskNote.addEventListener("click", () => {
+      console.log("Edit!");
+      editElement(taskNote);
+      document.getElementById("taskNoteList").replaceWith(loadNotes(task));
 
-    let deleteTaskButton = document.createElement('button');
-    deleteTaskButton.innerHTML = 'x';
-    deleteTaskButton.addEventListener('click', ()=>{
+    });
+
+    let deleteTaskButton = document.createElement("button");
+    deleteTaskButton.innerHTML = "x";
+    deleteTaskButton.addEventListener("click", (e) => {
+      e.stopPropagation();
       task.removeNote(notes[note]);
       console.log(task.notes);
-      document.getElementById('taskNoteList').replaceWith(loadNotes(task));
-    })
+      document.getElementById("taskNoteList").replaceWith(loadNotes(task));
+    });
     taskNote.appendChild(deleteTaskButton);
 
+    taskNote.addEventListener("click", editElement(taskNote));
     taskNoteList.appendChild(taskNote);
   }
 
   return taskNoteList;
 }
 
-function editNote(note){
+//take in a node
+//replace it with an input box
+//enter box "on enter" => replace the input box with the original node with the new value
+//working almost but need to figure out how to get it replacing properly
+function editElement(node) {
+  let originalNode = node;
+  let editBox = document.createElement("input");
+  node.replaceWith(editBox);
+  editBox.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
+      let newValue = editBox.value;
+      originalNode.innerHTML = newValue;
+      editBox.replaceWith(originalNode);
 
-
+    }
+  });
 }
